0ff702ebf7ddf6a7ee618d99e0a301fe88da469e
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-07-13 20:37:58 +0900

    24.07.13 localization en 추가

---
+++ Sources/LPNotiSys/Utility/SentData_en.swift
@@ -0,0 +1,8 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by 성재 on 7/13/24.
+//
+
+import Foundation
---
+++ Sources/LPNotiSys/Utility/SentData_ja.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 성재 on 7/13/24.
+//
+
+import Foundation
